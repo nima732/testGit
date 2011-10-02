a2d92e235d42d27a49d03bc6243f216011dc8737
Nima <nima7321@gmail.com>
2011-10-02 19:58:42 +0200

    kkk

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,5 +3,9 @@ public class MainClass {
 	public static void main(String args[]){
 		System.out.println("System.out.println");
 		System.out.println("System.out.println232");
+		
+		
+		
+		System.out.println("System.out.println TestGit3333");
 	}
 }
